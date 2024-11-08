f7fe44cc18d340a4413a15b620dd4bb63b8b80d6
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2024-11-08 16:49:27 +0100

    First commit

---
+++ src/main/java/org/gluns/glunsspring/GlunsSpringApplication.java
@@ -0,0 +1,13 @@
+package org.gluns.glunsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlunsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlunsSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/gluns/glunsspring/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.gluns.glunsspring;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GlunsSpringApplication.class);
+    }
+
+}
