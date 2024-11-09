d92a37f39490fda76338c00d84567b03f233f5d1
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2024-11-09 18:32:48 +0100

    Fix CORS

--- src/main/java/org/gluns/glunsspring/infrastructure/config/WebSecurityConfig.java
+++ src/main/java/org/gluns/glunsspring/infrastructure/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package org.gluns.glunsspring.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -8,10 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Used to define the methods that the WebSecurityConfig must implement.
  */
 @Configuration
-public class WebSecurityConfig implements WebMvcConfigurer {
+public class WebSecurityConfig implements WebFluxConfigurer {
     
     @Override
-    public void addCorsMappings(final org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+    public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
