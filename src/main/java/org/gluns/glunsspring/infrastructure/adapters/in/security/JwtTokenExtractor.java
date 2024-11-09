ba365def6ed0bde46e95ad694ba2367c2c0a2271
srodrigezSSD <srod>
2024-11-09 15:46:28 +0100

    Add JwtTokenExtractor service to get userId from JWT (Authorization)

---
+++ src/main/java/org/gluns/glunsspring/infrastructure/adapters/in/security/JwtTokenExtractor.java
@@ -0,0 +1,37 @@
+package org.gluns.glunsspring.infrastructure.adapters.in.security;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * Service for handling JWT tokens
+ */
+@Service
+public class JwtTokenExtractor {
+
+    public String getId(String authHeader) throws Exception {
+        String userId = "not found";
+        String userName = "not found";
+        DecodedJWT jwt = JWT.decode(authHeader.replace("Bearer", "").trim());
+
+        if (jwt.getClaim("sub") != null && !jwt.getClaim("sub").isNull()) {
+            userId = jwt.getClaim("sub").asString();
+        }
+
+        if (jwt.getClaim("preferred_username") != null && !jwt.getClaim("preferred_username").isNull()) {
+            userName = jwt.getClaim("preferred_username").asString();
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode json = mapper.createObjectNode();
+
+        json.put("userId", userId);
+        json.put("userName", userName);
+        return json.toString();
+    }
+
+}
--- src/main/java/org/gluns/glunsspring/infrastructure/rest/KeycloakController.java
+++ src/main/java/org/gluns/glunsspring/infrastructure/rest/KeycloakController.java
@@ -1,23 +1,32 @@
 package org.gluns.glunsspring.infrastructure.rest;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 
+import org.gluns.glunsspring.infrastructure.adapters.in.security.JwtTokenExtractor;
 import org.gluns.glunsspring.infrastructure.adapters.out.keycloak.KeycloakRestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/api/v1/iam")
+@Slf4j
 public class KeycloakController {
     @Autowired
     private KeycloakRestService restService;
+
+    @Autowired
+    private JwtTokenExtractor jwtTokenExtractor;
 
     @Operation(summary = "Login user", description = "Login user")
     @ApiResponse(responseCode = "200", description = "Login OK")
@@ -29,4 +38,20 @@ public class KeycloakController {
         return ResponseEntity.ok(login);
     }
     // TODO: add validation token
+
+    @Operation(summary = "Read JWT", description = "Read JWT")
+    @ApiResponse(responseCode = "200", description = "Login OK")
+    @ApiResponse(responseCode = "500", description = "Login error")
+    @GetMapping(value = "/login/token/id", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Hidden
+    public ResponseEntity<?> getUserId(@RequestHeader("Authorization") String authHeader) {
+        String userId = "not found";
+        try {
+            userId = jwtTokenExtractor.getId(authHeader);
+            return ResponseEntity.ok(userId);
+        } catch (Exception e) {
+            log.error("userId not found", e);
+        }
+        return ResponseEntity.ok(userId);
+    }
 }
